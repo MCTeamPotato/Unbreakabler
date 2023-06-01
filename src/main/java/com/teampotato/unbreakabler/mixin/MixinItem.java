94482f832526aa4a79132fdfe02b9a4c987867d5
Kasualix <670067575@qq.com>
2023-06-01 17:21:41 +0800

    fix #2

--- src/main/java/com/teampotato/unbreakabler/mixin/MixinItem.java
+++ src/main/java/com/teampotato/unbreakabler/mixin/MixinItem.java
@@ -2,7 +2,6 @@ package com.teampotato.unbreakabler.mixin;
 
 import com.teampotato.unbreakabler.Unbreakabler;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,10 +9,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Item.class, priority = 93)
-public class MixinItem {
+@Mixin(value = ItemStack.class, priority = 93)
+public abstract class MixinItem {
     @Inject(method = "inventoryTick", at = @At("HEAD"))
-    private void removeDurability(ItemStack stack, World world, Entity entity, int slot, boolean isSelected, CallbackInfo ci) {
-        Unbreakabler.giveUnbreakableTag(stack, world);
+    private void removeDurability(World world, Entity entity, int slot, boolean isSelected, CallbackInfo ci) {
+        Unbreakabler.giveUnbreakableTag((ItemStack) (Object)this, world);
     }
 }
