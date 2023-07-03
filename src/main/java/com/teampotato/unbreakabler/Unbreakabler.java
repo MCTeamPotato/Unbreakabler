90c83c72d9b950b77a4db6966955cc547653f9fb
Kasualix <670067575@qq.com>
2023-07-04 00:58:48 +0800

    1.1.0

--- src/main/java/com/teampotato/unbreakabler/Unbreakabler.java
+++ src/main/java/com/teampotato/unbreakabler/Unbreakabler.java
@@ -11,7 +11,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 @Mod(Unbreakabler.MOD_ID)
 public class Unbreakabler {
@@ -19,18 +18,16 @@ public class Unbreakabler {
 
     public static final ForgeConfigSpec COMMON_CONFIG;
     public static final ForgeConfigSpec.BooleanValue REMOVE_WEAPONS_AND_ARMORS_DURABILITY;
-    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS;
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS, VALID_NAMESPACE;
     public static final ForgeConfigSpec.ConfigValue<String> MODE;
 
     static {
-        List<? extends String> stringList = Lists.newArrayList();
-        Predicate<Object> validator = o -> o instanceof String;
-
         ForgeConfigSpec.Builder CONFIG_BUILDER = new ForgeConfigSpec.Builder();
-        CONFIG_BUILDER.comment("Unbreakabler").push("Unbreakable Age");
+        CONFIG_BUILDER.push("Unbreakabler");
         REMOVE_WEAPONS_AND_ARMORS_DURABILITY = CONFIG_BUILDER.define("enable mod", true);
-        MODE = CONFIG_BUILDER.comment("Use 'B' for Blacklist, use any other word(s) (e.g.  I love you) for whitelist").define("Blacklist or whitelist", "B");
-        BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS = CONFIG_BUILDER.defineList("Unbreakable tag blacklist/whitelist items", stringList, validator);
+        MODE = CONFIG_BUILDER.comment("Use 'B' for Blacklist mode, use any other word(s) (e.g.  I love you) for whitelist mode").define("Mode", "B");
+        BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS = CONFIG_BUILDER.comment("Unbreakable tag blacklist/whitelist items").defineList("Items", Lists.newArrayList(), o -> o instanceof String);
+        VALID_NAMESPACE = CONFIG_BUILDER.comment("If this is defined, only the mod that has this modID will be unbreakable.").defineList("ValidMods", Lists.newArrayList(), o -> o instanceof String);
         CONFIG_BUILDER.pop();
         COMMON_CONFIG = CONFIG_BUILDER.build();
     }
@@ -44,6 +41,7 @@ public class Unbreakabler {
         ResourceLocation location = ForgeRegistries.ITEMS.getKey(itemStack.getItem());
         if (location == null) return;
         String regName = location.toString();
+        if (!VALID_NAMESPACE.get().isEmpty() && !VALID_NAMESPACE.get().contains(location.getNamespace())) return;
         List<? extends String> list = BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS.get();
         if (MODE.get().equals("B")) {
             if (list.contains(regName)) return;
