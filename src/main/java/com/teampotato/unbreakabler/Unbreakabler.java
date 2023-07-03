586c0538d1e80b8e1aae8347f60368d1b3280536
Kasualix <670067575@qq.com>
2023-07-04 00:32:45 +0800

    1.1.0

--- src/main/java/com/teampotato/unbreakabler/Unbreakabler.java
+++ src/main/java/com/teampotato/unbreakabler/Unbreakabler.java
@@ -20,15 +20,16 @@ public class Unbreakabler {
 
     public static final ForgeConfigSpec COMMON_CONFIG;
     public static final ForgeConfigSpec.BooleanValue REMOVE_WEAPONS_AND_ARMORS_DURABILITY;
-    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS;
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS, VALID_NAMESPACE;
     public static final ForgeConfigSpec.ConfigValue<String> MODE;
 
     static {
         ForgeConfigSpec.Builder CONFIG_BUILDER = new ForgeConfigSpec.Builder();
         CONFIG_BUILDER.push("Unbreakabler");
         REMOVE_WEAPONS_AND_ARMORS_DURABILITY = CONFIG_BUILDER.define("enable mod", true);
-        MODE = CONFIG_BUILDER.comment("Use 'B' for Blacklist, use any other word(s) (e.g.  I love you) for whitelist").define("Blacklist or whitelist", "B");
-        BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS = CONFIG_BUILDER.defineList("Unbreakable tag blacklist/whitelist items", Lists.newArrayList(), o -> o instanceof String);
+        MODE = CONFIG_BUILDER.comment("Use 'B' for Blacklist mode, use any other word(s) (e.g.  I love you) for whitelist mode").define("Mode", "B");
+        BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS = CONFIG_BUILDER.comment("Unbreakable tag blacklist/whitelist items").defineList("Items", Lists.newArrayList(), o -> o instanceof String);
+        VALID_NAMESPACE = CONFIG_BUILDER.comment("If this is defined, only the mod that has this modID will be unbreakable.").defineList("ValidMods", Lists.newArrayList(), o -> o instanceof String);
         CONFIG_BUILDER.pop();
         COMMON_CONFIG = CONFIG_BUILDER.build();
     }
@@ -36,6 +37,7 @@ public class Unbreakabler {
     public static void giveUnbreakableTag(ItemStack itemStack, World world) {
         if (itemStack.isEmpty() || !itemStack.isDamageableItem() || !REMOVE_WEAPONS_AND_ARMORS_DURABILITY.get() || world.isClientSide || itemStack.getItem().getRegistryName() == null) return;
         String regName = itemStack.getItem().getRegistryName().toString();
+        if (!VALID_NAMESPACE.get().isEmpty() && !VALID_NAMESPACE.get().contains(itemStack.getItem().getRegistryName().getNamespace())) return;
         List<? extends String> list = BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS.get();
         if (MODE.get().equals("B")) {
             if (list.contains(regName)) return;
@@ -45,4 +47,3 @@ public class Unbreakabler {
         itemStack.getOrCreateTag().putBoolean("Unbreakable", true);
     }
 }
-
