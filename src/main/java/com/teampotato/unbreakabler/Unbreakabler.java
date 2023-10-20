997d06883df15040a79468a2b5d18350b8eb0875
Kasualix <670067575@qq.com>
2023-10-20 13:25:24 +0800

    1.2.0

--- src/main/java/com/teampotato/unbreakabler/Unbreakabler.java
+++ src/main/java/com/teampotato/unbreakabler/Unbreakabler.java
@@ -1,14 +1,10 @@
 package com.teampotato.unbreakabler;
 
-import com.google.common.collect.Lists;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.config.ModConfig;
 
 import java.util.List;
 
@@ -17,37 +13,25 @@ public class Unbreakabler {
     public static final String MOD_ID = "unbreakabler";
 
     public static final ForgeConfigSpec COMMON_CONFIG;
-    public static final ForgeConfigSpec.BooleanValue REMOVE_WEAPONS_AND_ARMORS_DURABILITY;
+    public static final ForgeConfigSpec.BooleanValue ENABLE_MOD, ONLY_WORK_ON_ARMORS, ONLY_WORKS_ON_WEAPONS;
     public static final ForgeConfigSpec.ConfigValue<List<? extends String>> BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS, VALID_NAMESPACE;
     public static final ForgeConfigSpec.ConfigValue<String> MODE;
 
     static {
         ForgeConfigSpec.Builder CONFIG_BUILDER = new ForgeConfigSpec.Builder();
         CONFIG_BUILDER.push("Unbreakabler");
-        REMOVE_WEAPONS_AND_ARMORS_DURABILITY = CONFIG_BUILDER.define("enable mod", true);
+        ENABLE_MOD = CONFIG_BUILDER.define("EnableMod", true);
+        ONLY_WORK_ON_ARMORS = CONFIG_BUILDER.define("OnlyWorkOnArmors", false);
+        ONLY_WORKS_ON_WEAPONS = CONFIG_BUILDER.define("OnlyWorkOnWeapons", false);
         MODE = CONFIG_BUILDER.comment("Use 'B' for Blacklist mode, use any other word(s) (e.g.  I love you) for whitelist mode").define("Mode", "B");
-        BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS = CONFIG_BUILDER.comment("Unbreakable tag blacklist/whitelist items").defineList("Items", Lists.newArrayList(), o -> o instanceof String);
-        VALID_NAMESPACE = CONFIG_BUILDER.comment("If this is defined, only the mod that has this modID will be unbreakable.").defineList("ValidMods", Lists.newArrayList(), o -> o instanceof String);
+        BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS = CONFIG_BUILDER.comment("Unbreakable tag blacklist/whitelist items").defineList("Items", new ObjectArrayList<>(), o -> o instanceof String);
+        VALID_NAMESPACE = CONFIG_BUILDER.comment("If this is defined, only the mod that has this modID will be unbreakable.").defineList("ValidMods", new ObjectArrayList<>(), o -> o instanceof String);
         CONFIG_BUILDER.pop();
         COMMON_CONFIG = CONFIG_BUILDER.build();
     }
-    
-    public Unbreakabler(){
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, COMMON_CONFIG);
+
+    public Unbreakabler() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, COMMON_CONFIG, MOD_ID + ".toml");
     }
 
-    public static void giveUnbreakableTag(ItemStack itemStack, Level world) {
-        if (itemStack.isEmpty() || !itemStack.isDamageableItem() || !REMOVE_WEAPONS_AND_ARMORS_DURABILITY.get() || world.isClientSide) return;
-        ResourceLocation location = ForgeRegistries.ITEMS.getKey(itemStack.getItem());
-        if (location == null) return;
-        String regName = location.toString();
-        if (!VALID_NAMESPACE.get().isEmpty() && !VALID_NAMESPACE.get().contains(location.getNamespace())) return;
-        List<? extends String> list = BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS.get();
-        if (MODE.get().equals("B")) {
-            if (list.contains(regName)) return;
-        } else {
-            if (!list.contains(regName)) return;
-        }
-        itemStack.getOrCreateTag().putBoolean("Unbreakable", true);
-    }
 }
--- src/main/java/com/teampotato/unbreakabler/mixin/MixinItemStack.java
+++ src/main/java/com/teampotato/unbreakabler/mixin/MixinItemStack.java
@@ -1,18 +1,62 @@
 package com.teampotato.unbreakabler.mixin;
 
 import com.teampotato.unbreakabler.Unbreakabler;
+import com.teampotato.unbreakabler.api.ExtendedItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ItemStack.class, priority = 93)
-public class MixinItemStack {
+import java.util.List;
+
+@Mixin(ItemStack.class)
+public abstract class MixinItemStack implements ExtendedItemStack {
+    @Shadow public abstract boolean isEmpty();
+    @Shadow public abstract boolean isDamageableItem();
+    @Shadow public abstract Item getItem();
+
+    @Shadow public abstract CompoundTag getOrCreateTag();
+
+    @Unique
+    private boolean unbreakabler$shouldBeUnbreakable;
+
     @Inject(method = "inventoryTick", at = @At("HEAD"))
-    private void removeDurability(Level world, Entity entity, int slot, boolean isSelected, CallbackInfo ci) {
-        Unbreakabler.giveUnbreakableTag((ItemStack)(Object)this, world);
+    private void removeDurability(Level world, Entity p_41668_, int p_41669_, boolean p_41670_, CallbackInfo ci) {
+        if (this.unbreakabler$shouldBeUnbreakable() && this.isDamageableItem()) {
+            this.getOrCreateTag().putBoolean("Unbreakable", true);
+            return;
+        }
+        Item item = this.getItem();
+        ResourceLocation id = ForgeRegistries.ITEMS.getKey(item);
+        if (!Unbreakabler.ENABLE_MOD.get() || world.isClientSide() || this.isEmpty() || id == null || !this.isDamageableItem()) return;
+        if (Unbreakabler.ONLY_WORKS_ON_WEAPONS.get() && !(item instanceof SwordItem || item instanceof AxeItem || item instanceof TridentItem)) return;
+        if (Unbreakabler.ONLY_WORK_ON_ARMORS.get() && !(item instanceof ArmorItem || item instanceof ElytraItem)) return;
+        if (!Unbreakabler.VALID_NAMESPACE.get().isEmpty() && !Unbreakabler.VALID_NAMESPACE.get().contains(id.getNamespace())) return;
+        String regName = id.toString();
+        List<? extends String> list = Unbreakabler.BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS.get();
+        if (Unbreakabler.MODE.get().equals("B")) {
+            if (list.contains(regName)) return;
+        } else {
+            if (!list.contains(regName)) return;
+        }
+        this.unbreakabler$setShouldBeUnbreakable(true);
+    }
+
+    @Override
+    public void unbreakabler$setShouldBeUnbreakable(boolean shouldBeUnbreakable) {
+        this.unbreakabler$shouldBeUnbreakable = shouldBeUnbreakable;
+    }
+
+    @Override
+    public boolean unbreakabler$shouldBeUnbreakable() {
+        return this.unbreakabler$shouldBeUnbreakable;
     }
 }
