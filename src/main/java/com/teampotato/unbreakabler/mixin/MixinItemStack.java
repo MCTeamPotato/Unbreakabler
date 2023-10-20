b3109013c61adb3cd23ed251fa7086055a9b84a4
Kasualix <670067575@qq.com>
2023-10-20 13:31:48 +0800

    fix on cache

--- src/main/java/com/teampotato/unbreakabler/mixin/MixinItemStack.java
+++ src/main/java/com/teampotato/unbreakabler/mixin/MixinItemStack.java
@@ -19,35 +19,38 @@ import java.util.List;
 
 @Mixin(ItemStack.class)
 public abstract class MixinItemStack implements ExtendedItemStack {
-    @Shadow public abstract boolean isEmpty();
+
     @Shadow public abstract boolean isDamageableItem();
-    @Shadow public abstract Item getItem();
 
     @Shadow public abstract CompoundTag getOrCreateTag();
 
-    @Unique
-    private boolean unbreakabler$shouldBeUnbreakable;
+    @Shadow public abstract Item getItem();
+
+    @Shadow public abstract boolean isEmpty();
+
+    @Unique private boolean unbreakabler$shouldBeUnbreakable;
+    @Unique private boolean unbreakabler$checked;
 
     @Inject(method = "inventoryTick", at = @At("HEAD"))
     private void removeDurability(Level world, Entity p_41668_, int p_41669_, boolean p_41670_, CallbackInfo ci) {
-        if (this.unbreakabler$shouldBeUnbreakable() && this.isDamageableItem()) {
-            this.getOrCreateTag().putBoolean("Unbreakable", true);
-            return;
+        if (this.unbreakabler$shouldBeUnbreakable() && this.isDamageableItem()) this.getOrCreateTag().putBoolean("Unbreakable", true);
+        if (!this.unbreakabler$checked()) {
+            this.unbreakabler$setChecked(true);
+            Item item = this.getItem();
+            ResourceLocation id = ForgeRegistries.ITEMS.getKey(item);
+            if (!Unbreakabler.ENABLE_MOD.get() || world.isClientSide() || this.isEmpty() || id == null || !this.isDamageableItem()) return;
+            if (Unbreakabler.ONLY_WORKS_ON_WEAPONS.get() && !(item instanceof SwordItem || item instanceof AxeItem || item instanceof TridentItem)) return;
+            if (Unbreakabler.ONLY_WORK_ON_ARMORS.get() && !(item instanceof ArmorItem || item instanceof ElytraItem)) return;
+            if (!Unbreakabler.VALID_NAMESPACE.get().isEmpty() && !Unbreakabler.VALID_NAMESPACE.get().contains(id.getNamespace())) return;
+            String regName = id.toString();
+            List<? extends String> list = Unbreakabler.BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS.get();
+            if (Unbreakabler.MODE.get().equals("B")) {
+                if (list.contains(regName)) return;
+            } else {
+                if (!list.contains(regName)) return;
+            }
+            this.unbreakabler$setShouldBeUnbreakable(true);
         }
-        Item item = this.getItem();
-        ResourceLocation id = ForgeRegistries.ITEMS.getKey(item);
-        if (!Unbreakabler.ENABLE_MOD.get() || world.isClientSide() || this.isEmpty() || id == null || !this.isDamageableItem()) return;
-        if (Unbreakabler.ONLY_WORKS_ON_WEAPONS.get() && !(item instanceof SwordItem || item instanceof AxeItem || item instanceof TridentItem)) return;
-        if (Unbreakabler.ONLY_WORK_ON_ARMORS.get() && !(item instanceof ArmorItem || item instanceof ElytraItem)) return;
-        if (!Unbreakabler.VALID_NAMESPACE.get().isEmpty() && !Unbreakabler.VALID_NAMESPACE.get().contains(id.getNamespace())) return;
-        String regName = id.toString();
-        List<? extends String> list = Unbreakabler.BLACKLIST_OR_WHITELIST_DAMAGEABLE_ITEMS.get();
-        if (Unbreakabler.MODE.get().equals("B")) {
-            if (list.contains(regName)) return;
-        } else {
-            if (!list.contains(regName)) return;
-        }
-        this.unbreakabler$setShouldBeUnbreakable(true);
     }
 
     @Override
@@ -58,5 +61,15 @@ public abstract class MixinItemStack implements ExtendedItemStack {
     @Override
     public boolean unbreakabler$shouldBeUnbreakable() {
         return this.unbreakabler$shouldBeUnbreakable;
+    }
+
+    @Override
+    public void unbreakabler$setChecked(boolean checked) {
+        this.unbreakabler$checked = checked;
+    }
+
+    @Override
+    public boolean unbreakabler$checked() {
+        return this.unbreakabler$checked;
     }
 }
