2136e75a17ae07194c7e17d39ae0867afd8e7211
Kasualix <670067575@qq.com>
2023-12-24 16:05:26 +0800

    finally......

--- src/main/java/com/teampotato/unbreakabler/Event.java
+++ src/main/java/com/teampotato/unbreakabler/Event.java
@@ -1,37 +1,42 @@
 package com.teampotato.unbreakabler;
 
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Event {
     private static final Map<Item, Boolean> ITEMS_CACHE = new HashMap<>();
-    @SubscribeEvent
+
+    private static final Logger LOGGER = LogManager.getLogger(Event.class);
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onPlayerUpdate(TickEvent.PlayerTickEvent event) {
         if (event.phase != TickEvent.Phase.START) return;
-        ItemStack itemInUse = event.player.getItemInUse();
-        if (itemInUse == null || itemInUse.stackSize == 0) return;
-        String name = itemInUse.getItem().getUnlocalizedName();
-        System.out.println(name);
+        ItemStack currentItem = event.player.inventory.getCurrentItem();
+        if (currentItem == null) return;
         if (!Config.enabled) return;
-        if (itemInUse.isItemStackDamageable() && ITEMS_CACHE.computeIfAbsent(itemInUse.getItem(), item -> {
+        boolean generateCache = ITEMS_CACHE.computeIfAbsent(currentItem.getItem(), item -> {
             if (Config.VALID_MODS.isEmpty()) {
-                if (Config.mode.equals("B")) {
-                    return Config.ITEMS.stream().noneMatch(string -> item.getUnlocalizedName().contains(string));
+                if (!Config.mode.equals("B")) {
+                    return !Config.ITEMS.contains(item.getUnlocalizedName());
                 } else {
-                    return Config.ITEMS.stream().anyMatch(string -> item.getUnlocalizedName().contains(string));
+                    return Config.ITEMS.contains(item.getUnlocalizedName());
                 }
             } else {
-                return Config.VALID_MODS.stream().anyMatch(string -> item.getUnlocalizedName().contains(string));
+                return !Config.VALID_MODS.contains(item.delegate.name().split(":")[0]);
             }
-        })) {
-            if (!itemInUse.hasTagCompound())itemInUse.setTagCompound(new NBTTagCompound());
-            itemInUse.getTagCompound().setBoolean("Unbreakble", true);
+        });
+        if (currentItem.isItemStackDamageable() && generateCache) {
+            if (!currentItem.hasTagCompound()) currentItem.setTagCompound(new NBTTagCompound());
+            currentItem.getTagCompound().setBoolean("Unbreakable", true);
         }
     }
 }
--- src/main/java/com/teampotato/unbreakabler/Unbreakabler.java
+++ src/main/java/com/teampotato/unbreakabler/Unbreakabler.java
@@ -5,20 +5,17 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Unbreakabler.MODID, dependencies = "after:spongemixins@[1.1.0,)", useMetadata = true)
+@Mod(modid = Unbreakabler.MODID, useMetadata = true)
 public class Unbreakabler {
     public static final String MODID = "unbreakabler";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        System.out.println("[Unbreakabler] pre-initialized successfully");
         Config.initConfig(event.getSuggestedConfigurationFile());
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("[Unbreakabler] initialized successfully");
-        Event playerEvent = new Event();
-        FMLCommonHandler.instance().bus().register(playerEvent);
+        FMLCommonHandler.instance().bus().register(new Event());
     }
 }
