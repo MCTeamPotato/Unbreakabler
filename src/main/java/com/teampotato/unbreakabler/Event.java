2ea2146a6530c3d97f9d7fc72d727fc40c26ba5e
Kasualix <670067575@qq.com>
2023-12-24 17:34:44 +0800

    OMG

--- src/main/java/com/teampotato/unbreakabler/Event.java
+++ src/main/java/com/teampotato/unbreakabler/Event.java
@@ -6,16 +6,12 @@ import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Event {
     private static final Map<Item, Boolean> ITEMS_CACHE = new HashMap<>();
-
-    private static final Logger LOGGER = LogManager.getLogger(Event.class);
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onPlayerUpdate(TickEvent.PlayerTickEvent event) {
@@ -25,13 +21,13 @@ public class Event {
         if (!Config.enabled) return;
         boolean generateCache = ITEMS_CACHE.computeIfAbsent(currentItem.getItem(), item -> {
             if (Config.VALID_MODS.isEmpty()) {
-                if (!Config.mode.equals("B")) {
+                if (Config.mode.equals("B")) {
                     return !Config.ITEMS.contains(item.getUnlocalizedName());
                 } else {
                     return Config.ITEMS.contains(item.getUnlocalizedName());
                 }
             } else {
-                return !Config.VALID_MODS.contains(item.delegate.name().split(":")[0]);
+                return Config.VALID_MODS.contains(item.delegate.name().split(":")[0]);
             }
         });
         if (currentItem.isItemStackDamageable() && generateCache) {
